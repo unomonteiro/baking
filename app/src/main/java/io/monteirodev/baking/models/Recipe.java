f071e8ba0fd2a4704cda76c866929c47e89c3f7f
Carlos Monteiro <offbyte@gmail.com>
2018-05-19 15:23:51 +0100

    add retrofit pojo classes

---
+++ app/src/main/java/io/monteirodev/baking/models/Recipe.java
@@ -0,0 +1,134 @@
+package io.monteirodev.baking.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recipe implements Parcelable {
+
+    //@SerializedName("id")
+    @Expose
+    private Integer id;
+    //@SerializedName("name")
+    @Expose
+    private String name;
+    //@SerializedName("ingredients")
+    @Expose
+    private ArrayList<Ingredient> ingredients = null;
+    //@SerializedName("steps")
+    @Expose
+    private ArrayList<Step> steps = null;
+    //@SerializedName("servings")
+    @Expose
+    private Integer servings;
+    //@SerializedName("image")
+    @Expose
+    private String image;
+
+    public Recipe() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public ArrayList<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(ArrayList<Step> steps) {
+        this.steps = steps;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (id == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeInt(id);
+        }
+        dest.writeString(name);
+        if (servings == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+        dest.writeInt(servings);
+        }
+        dest.writeString(image);
+    }
+
+    protected Recipe(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readInt();
+        }
+        name = in.readString();
+        if (in.readByte() == 0) {
+            servings = null;
+        } else {
+        servings = in.readInt();
+        }
+        image = in.readString();
+    }
+
+    public static final Creator<Recipe> CREATOR = new Creator<Recipe>() {
+        @Override
+        public Recipe createFromParcel(Parcel in) {
+            return new Recipe(in);
+        }
+
+        @Override
+        public Recipe[] newArray(int size) {
+            return new Recipe[size];
+        }
+    };
+
+}
