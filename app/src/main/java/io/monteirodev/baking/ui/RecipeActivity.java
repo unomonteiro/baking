e37e556b3638cad106a4f1e96e93c29fe3211bfd
Carlos Monteiro <offbyte@gmail.com>
2018-05-24 00:44:05 +0100

    Add recyclerView with ingredient list and step types

--- app/src/main/java/io/monteirodev/baking/ui/RecipeActivity.java
+++ app/src/main/java/io/monteirodev/baking/ui/RecipeActivity.java
@@ -1,18 +1,152 @@
 package io.monteirodev.baking.ui;
 
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.monteirodev.baking.R;
+import io.monteirodev.baking.database.BakingProvider;
+import io.monteirodev.baking.database.IngredientColumns;
+import io.monteirodev.baking.database.StepColumns;
+import timber.log.Timber;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor>, RecipeDetailsAdapter.StepClickListener {
 
     public static final String INTENT_EXTRA_RECIPE_ID = "intent_extra_recipe_id";
+    public static final String INTENT_EXTRA_RECIPE_NAME = "intent_extra_recipe_name";
+    private static final int ID_INGREDIENTS_LOADER = 2;
+    private static final int ID_STEPS_LOADER = 3;
+
+    @BindView(R.id.recipe_details_recycler_view)
+    RecyclerView mRecyclerView;
+    private int mPosition = RecyclerView.NO_POSITION;
+
+    private LinearLayoutManager mLayoutManager;
+    private RecipeDetailsAdapter mRecipeDetailsAdapter;
+    private int mRecipeId;
+
+    public static final String[] INGREDIENTS_PROJECTION = {
+            IngredientColumns.QUANTITY,
+            IngredientColumns.MEASURE,
+            IngredientColumns.INGREDIENT
+    };
+
+    public static final String[] STEPS_PROJECTION = {
+            StepColumns.ID,
+            StepColumns.STEP,
+            StepColumns.SHORT_DESCRIPTION
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
-        int recipeId = getIntent().getIntExtra(INTENT_EXTRA_RECIPE_ID, 0);
+        ButterKnife.bind(this);
+
+        String recipeName = getString(R.string.app_name);
+        if (getIntent() != null && getIntent().getExtras() != null) {
+            if (getIntent().hasExtra(INTENT_EXTRA_RECIPE_ID)) {
+                mRecipeId = getIntent().getIntExtra(INTENT_EXTRA_RECIPE_ID, 0);
+            }
+            if (getIntent().hasExtra(INTENT_EXTRA_RECIPE_ID)) {
+                recipeName = getIntent().getStringExtra(INTENT_EXTRA_RECIPE_NAME);
+            }
+        }
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setTitle(recipeName);
+        }
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecipeDetailsAdapter = new RecipeDetailsAdapter(this);
+        mRecyclerView.setAdapter(mRecipeDetailsAdapter);
+
+        if (mRecipeId > 0) {
+            getSupportLoaderManager().restartLoader(ID_INGREDIENTS_LOADER, null, this);
+            getSupportLoaderManager().restartLoader(ID_STEPS_LOADER, null, this);
+        }
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int loaderId, @Nullable Bundle args) {
+        switch (loaderId) {
+            case ID_INGREDIENTS_LOADER:
+                return new CursorLoader(this,
+                        BakingProvider.Ingredients.recipeIngredients(mRecipeId),
+                        INGREDIENTS_PROJECTION,
+                        null,
+                        null,
+                        null);
+
+            case ID_STEPS_LOADER:
+                return new CursorLoader(this,
+                        BakingProvider.Steps.recipeSteps(mRecipeId),
+                        STEPS_PROJECTION,
+                        null,
+                        null,
+                        null);
+
+            default:
+                throw new RuntimeException("Loader Not Implemented: " + loaderId);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        if (data != null && data.getCount() != 0) {
+            switch (loader.getId()) {
+                case ID_INGREDIENTS_LOADER:
+                    smoothCursorSwap(ID_INGREDIENTS_LOADER, data);
+                    break;
+                case ID_STEPS_LOADER:
+                    smoothCursorSwap(ID_STEPS_LOADER, data);
+                    break;
+                default:
+                    throw new RuntimeException("Loader Not Implemented: " + loader.getId());
+            }
+        }
+    }
+
+    private void smoothCursorSwap(int loaderId, Cursor data) {
+        if (ID_INGREDIENTS_LOADER == loaderId) {
+            mRecipeDetailsAdapter.swapIngredientsCursor(data);
+        } else if (ID_STEPS_LOADER == loaderId) {
+            mRecipeDetailsAdapter.swapStepsCursor(data);
+        }
+        if (mPosition == RecyclerView.NO_POSITION) mPosition = 0;
+        mRecyclerView.smoothScrollToPosition(mPosition);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        switch (loader.getId()) {
+            case ID_INGREDIENTS_LOADER:
+                mRecipeDetailsAdapter.swapIngredientsCursor(null);
+                break;
+            case ID_STEPS_LOADER:
+                mRecipeDetailsAdapter.swapStepsCursor(null);
+                break;
+            default:
+                throw new RuntimeException("Loader Not Implemented: " + loader.getId());
+        }
+    }
+
+    @Override
+    public void onStepClick(int stepId) {
+        Timber.d("Step clicked: " + stepId);
     }
 }
---
+++ app/src/main/java/io/monteirodev/baking/ui/RecipeDetailsAdapter.java
@@ -0,0 +1,196 @@
+package io.monteirodev.baking.ui;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.monteirodev.baking.R;
+import io.monteirodev.baking.database.IngredientColumns;
+import io.monteirodev.baking.database.StepColumns;
+
+public class RecipeDetailsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int INGREDIENTS_VIEW_TYPE = 0;
+    private static final int STEP_VIEW_TYPE = 1;
+
+    final private StepClickListener mStepClickListener;
+    private Cursor mIngredientsCursor;
+    private Cursor mStepsCursor;
+
+    public RecipeDetailsAdapter(StepClickListener stepClickListener) {
+        mStepClickListener = stepClickListener;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        int layoutId;
+
+        switch (viewType) {
+
+            case INGREDIENTS_VIEW_TYPE:
+                layoutId = R.layout.recipe_detail_ingredients_item;
+                break;
+
+            case STEP_VIEW_TYPE:
+                layoutId = R.layout.recipe_detail_step_item;
+                break;
+
+            default:
+                throw new IllegalArgumentException("Invalid view type, value of " + viewType);
+        }
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
+        view.setFocusable(true);
+
+        switch (viewType) {
+            case INGREDIENTS_VIEW_TYPE: {
+                return new IngredientsViewHolder(view);
+            }
+            case STEP_VIEW_TYPE: {
+                return new StepViewHolder(view);
+            }
+            default:
+                throw new IllegalArgumentException("Invalid view type, value of " + viewType);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Context context = holder.itemView.getContext();
+        int viewType = getItemViewType(position);
+        switch (viewType) {
+            case INGREDIENTS_VIEW_TYPE: {
+                IngredientsViewHolder ingredientsViewHolder = (IngredientsViewHolder) holder;
+                List<String> ingredientList = getIngredientList(context);
+                ingredientsViewHolder.mIngredientListTextView.setText(
+                        TextUtils.join("\n", ingredientList));
+                break;
+            }
+            case STEP_VIEW_TYPE: {
+                StepViewHolder stepViewHolder = (StepViewHolder) holder;
+                int adjustedPosition = position + (hasIngredientList() ? -1 : 0);
+                mStepsCursor.moveToPosition(adjustedPosition);
+                int stepNumber = mStepsCursor.getInt(mStepsCursor.getColumnIndex(
+                        StepColumns.STEP));
+                String shortDescription = mStepsCursor.getString(mStepsCursor.getColumnIndex(StepColumns.SHORT_DESCRIPTION));
+                if (stepNumber == 0) {
+                    stepViewHolder.mStepTextView.setText(shortDescription);
+                } else {
+                    String numberShortDescription = context.getString(
+                            R.string.number_step, stepNumber, shortDescription);
+                    stepViewHolder.mStepTextView.setText(numberShortDescription);
+                    stepViewHolder.itemView.setTag(mStepsCursor.getInt(mStepsCursor.getColumnIndex(StepColumns.ID)));
+                }
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("Invalid view type, value of " + viewType);
+        }
+    }
+
+    @NonNull
+    private List<String> getIngredientList(Context context) {
+        List<String> ingredientList = new ArrayList<>();
+        if (hasIngredientList() && context != null) {
+            while (mIngredientsCursor.moveToNext()) {
+                float quantityFloat = mIngredientsCursor.getFloat(
+                        mIngredientsCursor.getColumnIndex(IngredientColumns.QUANTITY));
+                DecimalFormat df = new DecimalFormat("0.##");
+                String quantity = df.format(quantityFloat);
+                String measure = mIngredientsCursor.getString(
+                        mIngredientsCursor.getColumnIndex(IngredientColumns.MEASURE));
+                String ingredient = mIngredientsCursor.getString(
+                        mIngredientsCursor.getColumnIndex(IngredientColumns.INGREDIENT));
+                ingredientList.add(context.getString(
+                        R.string.quantity_measure_ingredient, quantity, measure, ingredient));
+            }
+        }
+        return ingredientList;
+    }
+
+    @Override
+    public int getItemCount() {
+        int totalItems = 0;
+        if (hasIngredientList()) {
+            totalItems++;
+        }
+        totalItems += getStepsCount();
+        return totalItems;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0 && hasIngredientList()) {
+            return INGREDIENTS_VIEW_TYPE;
+        } else {
+            return STEP_VIEW_TYPE;
+        }
+    }
+
+    public void swapIngredientsCursor(Cursor newIngredientsCursor) {
+        mIngredientsCursor = newIngredientsCursor;
+        notifyDataSetChanged();
+    }
+
+    public void swapStepsCursor(Cursor newStepsCursor) {
+        mStepsCursor = newStepsCursor;
+        notifyDataSetChanged();
+    }
+
+    private boolean hasIngredientList() {
+        return mIngredientsCursor != null && mIngredientsCursor.getCount() > 0;
+    }
+
+    private int getStepsCount() {
+        if (mStepsCursor == null) {
+            return 0;
+        }
+        return mStepsCursor.getCount();
+    }
+
+    public interface StepClickListener {
+        void onStepClick(int stepId);
+    }
+
+    class IngredientsViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ingredients_title_text_view)
+        TextView mIngredientsTitleTextView;
+
+        @BindView(R.id.ingredient_list_text_view)
+        TextView mIngredientListTextView;
+
+        IngredientsViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    class StepViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.recipe_step_text_view)
+        TextView mStepTextView;
+
+        StepViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int stepId = (int) v.getTag();
+            mStepClickListener.onStepClick(stepId);
+        }
+    }
+}
