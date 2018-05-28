9c45c77ff93b3d92acc1181eec72d5ed11419a53
Carlos Monteiro <offbyte@gmail.com>
2018-05-28 14:08:23 +0100

    video rotation and life cycle refactoring

--- app/src/main/java/io/monteirodev/baking/ui/StepDetailFragment.java
+++ app/src/main/java/io/monteirodev/baking/ui/StepDetailFragment.java
@@ -2,7 +2,6 @@ package io.monteirodev.baking.ui;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -15,19 +14,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.dash.DashChunkSource;
-import com.google.android.exoplayer2.source.dash.DashMediaSource;
-import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
@@ -48,8 +44,7 @@ import static android.view.View.VISIBLE;
 public class StepDetailFragment extends Fragment {
     private static final String STEPS_KEY = "steps_key";
     private static final String STEP_INDEX_KEY = "step_index_key";
-
-    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+    private static final String PLAYER_POSITION = "player_position";
 
     private OnStepChangeListener mOnStepChangeListener;
 
@@ -71,26 +66,12 @@ public class StepDetailFragment extends Fragment {
 
     private SimpleExoPlayer mPlayer;
 
-    private long playbackPosition;
-    private int currentWindow;
+    private long mPlayerPosition;
     private boolean playWhenReady = true;
     private String mVideoURL;
 
     public StepDetailFragment() {
         // Required empty public constructor
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_step_detail, container, false);
-        unbinder = ButterKnife.bind(this, rootView);
-        if(savedInstanceState != null) {
-            mSteps = savedInstanceState.getParcelableArrayList(STEPS_KEY);
-            mStepIndex = savedInstanceState.getInt(STEP_INDEX_KEY);
-        }
-        updateStepViews();
-        return rootView;
     }
 
     @Override
@@ -105,9 +86,59 @@ public class StepDetailFragment extends Fragment {
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mOnStepChangeListener = null;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_step_detail, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+        if (savedInstanceState != null) {
+            mSteps = savedInstanceState.getParcelableArrayList(STEPS_KEY);
+            mStepIndex = savedInstanceState.getInt(STEP_INDEX_KEY);
+            mPlayerPosition = savedInstanceState.getLong(PLAYER_POSITION, C.TIME_UNSET);
+        } else {
+            mPlayerPosition = C.TIME_UNSET;
+        }
+        updateStepViews();
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        initializePlayer();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if (mPlayer != null) {
+            if (mPlayerPosition != C.TIME_UNSET) {
+                mPlayer.seekTo(mPlayerPosition);
+            }
+            mPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mPlayer != null) {
+            mPlayerPosition = mPlayer.getCurrentPosition();
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelableArrayList(STEPS_KEY, mSteps);
+        outState.putInt(STEP_INDEX_KEY, mStepIndex);
+        outState.putLong(PLAYER_POSITION, mPlayerPosition);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releasePlayer();
     }
 
     @Override
@@ -120,75 +151,15 @@ public class StepDetailFragment extends Fragment {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        if (Util.SDK_INT > 23) {
-            initializePlayer();
-        }
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        hideSystemUi();
-        if ((Util.SDK_INT <= 23 || mPlayer == null)) {
-            initializePlayer();
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (Util.SDK_INT <= 23) {
-            releasePlayer();
-        }
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (Util.SDK_INT > 23) {
-            releasePlayer();
-        }
-    }
-
-    private void releasePlayer() {
-        if (mPlayer != null) {
-            playbackPosition = mPlayer.getCurrentPosition();
-            currentWindow = mPlayer.getCurrentWindowIndex();
-            playWhenReady = mPlayer.getPlayWhenReady();
-            mPlayer.release();
-            mPlayer = null;
-        }
-    }
-
-    private MediaSource buildMediaSource(Uri uri) {
-        return new ExtractorMediaSource.Factory(
-                new DefaultHttpDataSourceFactory("exoplayer-codelab")).
-                createMediaSource(uri);
-    }
-
-    private MediaSource buildMediaSource4(Uri uri) {
-        DataSource.Factory manifestDataSourceFactory = new DefaultHttpDataSourceFactory("ua");
-        DashChunkSource.Factory dashChunkSourceFactory = new DefaultDashChunkSource.Factory(
-                new DefaultHttpDataSourceFactory("ua", BANDWIDTH_METER));
-        return new DashMediaSource.Factory(dashChunkSourceFactory, manifestDataSourceFactory)
-                .createMediaSource(uri);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void hideSystemUi() {
-        // todo
-        FragmentActivity activity = getActivity();
-        if (activity != null && activity.getResources().getConfiguration()
-                .orientation == ORIENTATION_LANDSCAPE) {
-            mPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
+    public void onDetach() {
+        super.onDetach();
+        mOnStepChangeListener = null;
     }
 
     public void setSteps(ArrayList<Step> steps) {
@@ -197,6 +168,22 @@ public class StepDetailFragment extends Fragment {
 
     public void setStepIndex(int stepIndex) {
         mStepIndex = stepIndex;
+    }
+
+    @OnClick(R.id.previous_button)
+    void previousStep() {
+        mStepIndex --;
+        releasePlayer();
+        updateStepViews();
+        initializePlayer();
+    }
+
+    @OnClick(R.id.next_button)
+    void nextStep() {
+        mStepIndex++;
+        releasePlayer();
+        updateStepViews();
+        initializePlayer();
     }
 
     private void updateStepViews() {
@@ -232,42 +219,50 @@ public class StepDetailFragment extends Fragment {
         if (mPlayer == null) {
             mPlayer = ExoPlayerFactory.newSimpleInstance(
                     new DefaultRenderersFactory(getContext()),
-                    new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(BANDWIDTH_METER)),
+                    new DefaultTrackSelector(
+                            new AdaptiveTrackSelection.Factory(
+                                    new DefaultBandwidthMeter())),
                     new DefaultLoadControl());
             mPlayerView.requestFocus();
             mPlayerView.setPlayer(mPlayer);
             mPlayer.setPlayWhenReady(playWhenReady);
+            MediaSource mediaSource = buildMediaSource(Uri.parse(mVideoURL));
+            mPlayer.prepare(mediaSource);
         }
-        MediaSource mediaSource = buildMediaSource(Uri.parse(mVideoURL));
-        mPlayer.prepare(mediaSource);
-        mThumbnailImageView.setVisibility(VISIBLE);
+        mThumbnailImageView.setVisibility(INVISIBLE);
         mPlayerView.setVisibility(VISIBLE);
     }
 
-
-    @OnClick(R.id.previous_button)
-    void previousStep() {
-        mStepIndex --;
-        releasePlayer();
-        updateStepViews();
-        initializePlayer();
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource.Factory(
+                new DefaultHttpDataSourceFactory("ua"))
+                .createMediaSource(uri);
     }
 
-    @OnClick(R.id.next_button)
-    void nextStep() {
-        mStepIndex++;
-        releasePlayer();
-        updateStepViews();
-        initializePlayer();
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        // todo
+        FragmentActivity activity = getActivity();
+        if (activity != null && activity.getResources().getConfiguration()
+                .orientation == ORIENTATION_LANDSCAPE) {
+            mPlayerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    }
+
+    private void releasePlayer() {
+        if (mPlayer != null) {
+            playWhenReady = mPlayer.getPlayWhenReady();
+            mPlayer.release();
+            mPlayer = null;
+        }
     }
 
     public interface OnStepChangeListener {
         void onStepChange(Uri uri);
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putParcelableArrayList(STEPS_KEY, mSteps);
-        outState.putInt(STEP_INDEX_KEY, mStepIndex);
     }
 }
