225dcc95e69017307f863e6f86d68a4f26c7eadd
Carlos Monteiro <offbyte@gmail.com>
2018-06-03 15:57:53 +0100

    Test ActivityMain

--- app/src/androidTest/java/io/monteirodev/baking/ui/MainActivityTest.java
+++ app/src/androidTest/java/io/monteirodev/baking/ui/MainActivityTest.java
@@ -1,17 +1,13 @@
 package io.monteirodev.baking.ui;
 
 
-import android.support.test.espresso.ViewInteraction;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,13 +16,13 @@ import io.monteirodev.baking.R;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
+import static io.monteirodev.baking.TestUtils.matchToolbarTitle;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -35,240 +31,30 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
+
+    /** <a href="https://stackoverflow.com/a/27669809/6997703">
+     *  How to click on an item inside a RecyclerView in Espresso
+     *  </a> */
     @Test
-    public void mainActivityTest() {
-        ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.recipes_recycler_view),
-                        childAtPosition(
-                                withClassName(is("android.widget.FrameLayout")),
-                                0)));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
-
-        ViewInteraction recyclerView2 = onView(
-                allOf(withId(R.id.recipes_recycler_view),
-                        childAtPosition(
-                                withClassName(is("android.widget.FrameLayout")),
-                                0)));
-        recyclerView2.perform(actionOnItemAtPosition(0, click()));
-
-        ViewInteraction recyclerView3 = onView(
-                allOf(withId(R.id.recipe_details_recycler_view),
-                        childAtPosition(
-                                withClassName(is("android.widget.FrameLayout")),
-                                0)));
-        recyclerView3.perform(actionOnItemAtPosition(1, click()));
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(4976);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(711);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatButton2 = onView(
-                allOf(withId(R.id.previous_button), withText("Previous"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                4),
-                        isDisplayed()));
-        appCompatButton2.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatButton3 = onView(
-                allOf(withId(R.id.previous_button), withText("Previous"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                4),
-                        isDisplayed()));
-        appCompatButton3.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatButton4 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton4.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatButton5 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton5.perform(click());
-
-        ViewInteraction appCompatButton6 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton6.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatButton7 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton7.perform(click());
-
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        ViewInteraction appCompatButton8 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton8.perform(click());
-
-        ViewInteraction appCompatButton9 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton9.perform(click());
-
-        ViewInteraction appCompatButton10 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton10.perform(click());
-
-        ViewInteraction appCompatButton11 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton11.perform(click());
-
-        ViewInteraction appCompatButton12 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton12.perform(click());
-
-        ViewInteraction appCompatButton13 = onView(
-                allOf(withId(R.id.next_button), withText("Next"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.step_container),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton13.perform(click());
-
+    public void actionBar_hasAppName() {
+        CharSequence title = InstrumentationRegistry.getTargetContext()
+                .getString(R.string.app_name);
+        matchToolbarTitle(title).check(matches(isDisplayed()));
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
+    @Test
+    public void recipesRecyclerView_isClickable() {
+        onView(withId(R.id.recipes_recycler_view))
+                .perform(actionOnItemAtPosition(0, click()));
+    }
 
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+    @Test
+    public void clickBrownies_opensRecipeActivityWithBrownies() {
+        // to do add dynamic click
+        String BROWNIES = "Brownies";
+        onView(withId(R.id.recipes_recycler_view))
+                .perform(RecyclerViewActions.actionOnItem(
+                        hasDescendant(withText(BROWNIES)), click()));
+        matchToolbarTitle(BROWNIES).check(matches(isDisplayed()));
     }
 }
